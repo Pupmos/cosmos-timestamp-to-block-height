f4b128c1855593db0431a58a70487e46c4a33253
siriustaikun <siriustaikun@protonmail.com>
2022-09-06 20:24:38 +0000

    ui mvp

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,31 @@
 import Head from "next/head";
-import Header from "@components/Header";
-import Footer from "@components/Footer";
+import FooterContent from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
 import JokeBlock from "@components/JokeBlock";
 import { BlockSearchForm } from "@components/BlockSearchForm"
+import { Card, Col, Divider, PageHeader, Row, Typography } from "antd";
+import { Footer } from "antd/lib/layout/layout";
+
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Timestamp to Cosmos Block Height Converter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <Header title="Next.js Toolbox" />
-        <BlockSearchForm></BlockSearchForm>
-        <FeedbackForm />
-        <JokeBlock />
-      </main>
-      <Footer />
-    </div>
+      {/* <Row justify="center"> */}
+        {/* <Col xs={12}> */}
+          <Card title={<>
+            Convert any timestamp to a Cosmos block height
+          </>}>
+            <BlockSearchForm></BlockSearchForm>
+          </Card>
+        {/* </Col> */}
+      {/* </Row> */}
+      {/* <Footer>
+        <FooterContent/>
+      </Footer> */}
+    </>
   );
 }
